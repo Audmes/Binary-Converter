fc8bb6908da873b00be9749fceeb63d041e31357
Audmes <102157665+Audmes@users.noreply.github.com>
2023-12-22 17:27:20 +0100

    Commit : 22/12/2023

--- script.js
+++ script.js
@@ -1,31 +1,80 @@
 // Convert to Binary
 function convertToBinary() {
-    let decimalNumber = document.getElementById('decimalInput').value;
+    let decimalInput = document.getElementById('decimalInput').value;
     let binaryResult = document.getElementById('binaryResult');
-    binaryResult.innerHTML = (+decimalNumber).toString(2);
 
-    // Or
-    // Convert decimal number to binary string
-    // let num = 42;
-    // let sbin = num.toString(2); // Specify radix 2 for binary
-    // console.log(sbin); // "101010"
+    // Vérifier si l'entrée est un nombre valide
+    if (!isNaN(decimalInput) && decimalInput !== '') {
+	    decimal = parseInt(decimalInput);
+	    let binary = '';
+
+        // Convertir le nombre décimal en binaire
+	    while (decimal > 0) {
+            binary = (decimal % 2) + binary;
+            decimal = Math.floor(decimal / 2);
+        }
+
+        // Afficher le résultat
+        binaryResult.textContent = binary;
+	    // binaryResult.textContent = `La représentation binaire est : ${binary}`;
+    }else {
+        binaryResult.textContent = "Entrez un nombre décimal valide.";
+    }
 }
 
 // Convert to Decimal
 function convertToDecimal() {
     // Convert binary string to decimal number
-    let binaryNumber = document.getElementById('binaryInput').value;
+    // let binaryNumber = document.getElementById('binaryInput').value;
+
+    let binaryInput = document.getElementById('binaryInput').value;
+    let decimalResult = document.getElementById('decimalResult');
     // let binaryNumber = "101010";
-    test = binaryNumber.indexOf('0');
-    console.log(test);
 
-    // if (browserType.indexOf("mozilla") !== -1) {
-    if ( ( binaryNumber.indexOf('1') == -1 ) || ( binaryNumber.indexOf('0') == 0 ) ) {
-        return decimalResult.innerHTML = 'Veuillez entrer un nombre binaire !';
+    // Vérifier si l'entrée est un nombre valide
+    if (!isNaN(binaryInput) && binaryInput !== '') {
+
+        let decimal = '';
+	    
+        // Convertir le nombre binaire en décimal
+        decimal = parseInt(binaryInput, 2);
+
+        if (!isNaN(decimal)) {
+            // Afficher le résultat
+            decimalResult.textContent = decimal;
+        }else {
+            decimalResult.textContent = "Entrez un nombre binaire valide.";
+        }   
+    }else {
+        decimalResult.textContent = "Entrez un nombre binaire valide.";
     }
-    
-    let decimal = parseInt(binaryNumber, 2); // Specify radix 2 for binary
-
-    return decimalResult.innerHTML = decimal;
-    //console.log(decimal); // 42 
 }
+
+// Convert String to Binary number
+// function convertTextToBinary() {
+//     let txt = document.getElementById('txtInput').value;
+
+//     function toBinary(l) {
+//         let bytes = 32;
+//         let num = [128,64,32,16,8,4,2,1];//8
+//         let c = l.charCodeAt()-bytes;
+//         let tab = "";
+         
+//         while (c != 0){
+//         for(var i = 0; i < num.length; i++){
+//             if((c-num[i]) >= 0){
+//                 tab += "1";
+//                 c = (c-num[i]);
+//             } else {
+//                 tab += "0";
+//             }
+//         }
+//         }
+//         return tab;
+//     }
+
+//     console.log(toBinary("Audrey"));
+//     return textBinaryResult.innerHTML = toBinary(txt);
+// }
+
+// Convert Binary to Text
